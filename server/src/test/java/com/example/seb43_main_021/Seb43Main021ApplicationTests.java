5fa02c4259578c181acfeceabac24923fd56bd49
jae-jung-kim <podosi158@gmail.com>
2023-05-03 16:00:01 +0900

    BACKEND

---
+++ server/src/test/java/com/example/seb43_main_021/Seb43Main021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seb43_main_021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb43Main021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/example/seb43_main_021/Seb43Main021Application.java
@@ -0,0 +1,13 @@
+package com.example.seb43_main_021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb43Main021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb43Main021Application.class, args);
+	}
+
+}
