00d56e50e411a27dca8989727b2426d722a7a176
min4415 <120303298+min4415@users.noreply.github.com>
2023-05-03 16:22:01 +0900

    Add files via upload

---
+++ seb_43_21_main_project/src/test/java/com/codestates/seb_43_21_main_project/Seb4321MainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.seb_43_21_main_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb4321MainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
