0c1304e4df36f230490bb2b20d98ef24797e83fc
dobbykong <wlk159@naver.com>
2023-05-04 21:23:50 +0900

    이미지 컴포넌트 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,53 @@
 import Images from "./components/UI/Item/Images";
+import styled from "styled-components";
 
-function App() {
-  return <Images />;
+const App = () => {
+  return (
+    <ItemContainer>
+      <ItemBox>
+      <Images />
+      <ItemDesc>
+        <h2>LG 제습기 팝니다.</h2>
+        <div>250,000원</div>
+        <p>제주 제주시 연동</p>
+      </ItemDesc>
+      </ItemBox>
+    </ItemContainer>
+  )
 }
+
+const ItemContainer = styled.div`
+width: calc(25% - 44px);
+margin-right: 44px;
+margin-bottom: 56px;
+`;
+
+const ItemBox = styled.div`
+text-decoration: none;
+color: #212529;
+`;
+
+const ItemDesc = styled.div`
+margin-top: 12px;
+
+div {
+ font-size: 15px;
+ font-weight: 700;
+ line-height: 1.5;
+ margin-bottom: 4px;
+}
+
+p {
+  font-size: 16px;
+  letter-spacing: -0.02px;
+  color: #212529;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  margin-bottom: 4px;
+  line-height: 1.5;
+  font-weight: normal;
+}
+`;
 
 export default App;
--- client/src/components/UI/Item/Images.js
+++ client/src/components/UI/Item/Images.js
@@ -1,6 +1,8 @@
 import myImage from '../../../assets/image2.jpg';
 import styled from 'styled-components'
 
+
+// 이미지 alt에 등록한 물품 제목 넣기 ex) alt={title}
 const Images = () => {
     return (
         <ImageContainer>
@@ -19,14 +21,14 @@ const ImageContainer = styled.div`
     box-shadow: inset 0px 0px 1px rgba(0, 0, 0, 0.15);
     box-sizing: border-box;
     img {
-        width: 100%;
         position: absolute;
-        top: 0;
-        bottom: 0;
-        box-sizing: border-box;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        max-width: 100%;
+        height: auto;
         border-radius: 12px;
         border: 1px solid transparent;
-        object-fit: contain;
     }
 `;
 
