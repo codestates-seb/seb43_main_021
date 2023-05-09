8f38aecc0100233a784dc9f48943a1c1caa6a382
taeyoung1012 <dlaxodud12@naver.com>
2023-05-09 17:40:19 +0900

    [Design] AuctionDetail 페이지 UI 구현

---
+++ client/src/assets/dummyData.jsx
@@ -0,0 +1,34 @@
+import img1 from "./img1.jpeg";
+import img2 from "./img2.jpg";
+import img3 from "./img3.jpg";
+
+export const dummyImages = [img1, img2, img3];
+
+export const dummyBiddings = [
+  {
+    id: 1,
+    img: img1,
+    itemTitle: "나이키 조던1 스캇 265",
+  },
+  {
+    id: 2,
+    img: img2,
+    itemTitle: "투애니원 앨범",
+  },
+  {
+    id: 3,
+    img: img3,
+    itemTitle: "찌개 책자",
+  },
+];
+
+export const dummyAuctions = {
+  userName: "당근단군",
+  userLocation: "군자동",
+  auctionTitle: "나이키 에어조던1 스캇 팬텀블랙 265",
+  auctionTime: "50분 전",
+  content:
+    "나이키 공홈에서 당첨된 나이키 에어조던1 스캇입니다!\n사진 찍기 위하여 보자기만 걷어봤고 그 외에는 시착도 안 했습니다.\n건대입구에서 직거래 희망합니다!",
+  auctionSummary: "입찰 3・관심 1・조회 146",
+  auctionEnd: "2023년 5월 15일 오후 5시 26분",
+};
--- client/src/pages/AuctionDetail.js
+++ client/src/pages/AuctionDetail.js
@@ -1,75 +1,67 @@
 import React from "react";
 import styled from "styled-components";
-import img21 from "../assets/158893.jpg";
-import imgNike from "../assets/nike.jpeg";
+import { RiHeartLine } from "react-icons/ri";
+import ItemImage from "../components/UI/ItemImage/ItemImage";
+import { dummyImages, dummyBiddings, dummyAuctions } from "../assets/dummyData";
 
 const AuctionDetail = () => {
   return (
-    <Container>
+    <Wrapper>
       <AuctionImgContainer>
-        <AuctionImg src={imgNike} />
+        <ItemImage images={dummyImages} />
       </AuctionImgContainer>
       <UserInfoContainer>
-        <UserImg></UserImg>
+        <UserImg />
         <UserText>
-          <UserTitle>당근단군</UserTitle>
-          <UserLocation>군자동</UserLocation>
+          <div>{dummyAuctions.userName}</div>
+          <div className="userLocation">{dummyAuctions.userLocation}</div>
         </UserText>
       </UserInfoContainer>
       <UserInfoUnderLine />
-      <AuctionTitle>나이키 에어조던1 스캇 팬텀블랙 265 </AuctionTitle>
-      <AutcionInfo>남성패션/잡화・50분 전</AutcionInfo>
-      <AuctionContent>
-        나이키 공홈에서 당첨된 나이키 에어조던1 스캇입니다!
-        <br />
-        <br /> 사진 찍기 위하여 보자기만 걷어봤고 그 외에는 시착도 안 했습니다.
-        <br /> <br />
-        건대입구에서 직거래 희망합니다!
-      </AuctionContent>
-      <AuctionSummary>입찰 3・관심 1・조회 146</AuctionSummary>
+      <AuctionTitle>{dummyAuctions.auctionTitle} </AuctionTitle>
+      <AutcionInfo>{dummyAuctions.auctionTime}</AutcionInfo>
+      <AuctionContent>{dummyAuctions.content}</AuctionContent>
+      <AuctionSummary>{dummyAuctions.auctionSummary}</AuctionSummary>
       <AuctionUnderLine />
       <BiddingList>입찰 목록</BiddingList>
       <BiddingItemGrid>
-        <BiddingItem>
-          <ItemImg src={img21} />
-          <ItemTitle>투애니원 앨범</ItemTitle>
-        </BiddingItem>
-        <BiddingItem>
-          <ItemImg src={img21} />
-          <ItemTitle>투애니원 앨범</ItemTitle>
-        </BiddingItem>
-        <BiddingItem>
-          <ItemImg src={img21} />
-          <ItemTitle>투애니원 앨범</ItemTitle>
-        </BiddingItem>
+        {dummyBiddings.map((item) => (
+          <BiddingItem key={item.id}>
+            <ItemImg src={item.img} />
+            <ItemTitle>{item.itemTitle}</ItemTitle>
+          </BiddingItem>
+        ))}
       </BiddingItemGrid>
       <Footer>
         <FooterLine />
+        <FooterContainer>
+          <Favorite />
+          <DivisionLine />
+          <AuctionEnd>
+            <div>경매 마감일</div>
+            <div style={{ color: "gray" }}>{dummyAuctions.auctionEnd}</div>
+          </AuctionEnd>
+          <BiddingButton>입찰하기</BiddingButton>
+        </FooterContainer>
       </Footer>
-    </Container>
+    </Wrapper>
   );
 };
 
-const Container = styled.div`
+// ============================================================================
+
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 1rem;
+  margin-bottom: 6rem;
 `;
 
 const AuctionImgContainer = styled.div`
   width: 100%;
-  padding-top: 100%;
   position: relative;
+  padding-top: 100%;
 `;
 
-const AuctionImg = styled.img`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
 const UserInfoContainer = styled.div`
   height: 5rem;
   display: flex;
@@ -87,18 +79,14 @@ const UserText = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-`;
-
-const UserTitle = styled.div`
+  font-weight: bold;
   margin-top: 1.5rem;
-  font-weight: bold;
-`;
-
-const UserLocation = styled.div`
   margin-bottom: 1.5rem;
-  font-weight: bold;
-  font-size: 0.75rem;
-  color: gray;
+
+  .userLocation {
+    font-size: 0.75rem;
+    color: gray;
+  }
 `;
 
 const UserInfoUnderLine = styled.div`
@@ -152,11 +140,12 @@ const BiddingItemGrid = styled.div`
 const BiddingItem = styled.div`
   display: flex;
   flex-direction: column;
+  margin-bottom: -15%;
 `;
 
 const ItemImg = styled.img`
-  width: 11.5rem;
-  height: 8.625rem;
+  width: 100%;
+  height: 75%;
   object-fit: cover;
   border-radius: 5px;
 `;
@@ -168,10 +157,126 @@ const ItemTitle = styled.div`
 
 const Footer = styled.div`
   height: 5rem;
+  width: 100%;
+  bottom: 0;
+  position: fixed;
+  background-color: white;
 `;
 
 const FooterLine = styled.div`
   border: 0.5px solid #cccccc;
 `;
 
+const FooterContainer = styled.div`
+  display: flex;
+`;
+
+const Favorite = styled(RiHeartLine)`
+  font-size: 1.5rem;
+  color: gray;
+  margin-left: 1rem;
+  margin-top: 1.5rem;
+  cursor: pointer;
+`;
+
+const DivisionLine = styled.div`
+  border: 0.5px solid #cccccc;
+  width: 0;
+  height: 2.5rem;
+  margin-top: 1rem;
+  margin-left: 1rem;
+`;
+
+const AuctionEnd = styled.div`
+  margin-top: 1.1rem;
+  margin-left: 1rem;
+  font-weight: bold;
+`;
+
+const BiddingButton = styled.button`
+  background-color: #4636fc;
+  color: white;
+  border: none;
+  width: 5.5rem;
+  height: 2.5rem;
+  font-weight: bold;
+  border-radius: 5px;
+  margin: 1rem 1rem 1rem auto;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #5170fd;
+  }
+`;
+
 export default AuctionDetail;
+
+// const AuctionImg = styled.img`
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 100%;
+//   object-fit: cover;
+// `;
+
+// const ArrowRight = styled.div`
+//   position: absolute;
+//   top: 45%;
+//   right: 3%;
+//   font-size: 3rem;
+//   color: white;
+//   cursor: pointer;
+// `;
+
+// const ArrowBack = styled.div`
+//   position: absolute;
+//   top: 45%;
+//   left: 3%;
+//   font-size: 3rem;
+//   color: white;
+//   cursor: pointer;
+// `;
+// const DotContainer = styled.div`
+//   position: absolute; /* 추가: 절대 위치 설정 */
+//   top: 0%; /* 추가: 원하는 위치로 조정 */
+//   left: 0%; /* 추가: 원하는 위치로 조정 */
+//   display: flex;
+// `;
+
+// const [currentImage, setCurrentImage] = useState(0);
+// const images = [imgNike, img, img1];
+
+// const nextImage = () => {
+//   const nextIndex = (currentImage + 1) % images.length;
+//   setCurrentImage(nextIndex);
+// };
+
+// const backImage = () => {
+//   const backIndex = (currentImage + images.length - 1) % images.length;
+//   setCurrentImage(backIndex);
+// };
+
+// <AuctionImg src={images[currentImage]} />
+//       <ArrowRight onClick={nextImage}>
+//         <IoIosArrowForward />
+//       </ArrowRight>
+//       <ArrowBack onClick={backImage}>
+//         <IoIosArrowBack />
+//       </ArrowBack>
+//       <DotContainer>
+//         {images.map((_, index) => (
+//           <Dot key={index} index={index} active={index === currentImage} />
+//         ))}
+//       </DotContainer>
+
+// const Dot = styled.div`
+//   position: absolute;
+//   bottom: 5%;
+//   left: ${(props) => props.index * 5}%;
+//   background-color: ${(props) => (props.active ? "white" : "#818181")};
+//   width: 3%;
+//   height: 3%;
+//   border-radius: 50%;
+//   opacity: 70%;
+// `;
