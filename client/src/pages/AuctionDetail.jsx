7239b852c4edcb90a8ccaac738f078935c5c6913
taeyoung1012 <dlaxodud12@naver.com>
2023-05-10 03:05:56 +0900

    [Feat] AuctionDetail, BiddingDetail useNavigate 구현

--- client/src/pages/AuctionDetail.jsx
+++ client/src/pages/AuctionDetail.jsx
@@ -1,33 +1,69 @@
 import React from "react";
 import styled from "styled-components";
 import { RiHeartLine } from "react-icons/ri";
+import { IoArrowBackOutline } from "react-icons/io5";
 import ItemImage from "../components/UI/ItemImage/ItemImage";
-import { dummyImages, dummyBiddings, dummyAuctions } from "../assets/dummyData";
+import { dummyBiddings, dummyItem } from "../assets/dummyData";
+import { useNavigate, useParams } from "react-router-dom";
 
 const AuctionDetail = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const handleBack = () => {
+    window.history.back();
+  };
+
+  const handleToBiddingDetail = (id) => {
+    navigate(`/biddingdetail/${id}`);
+  };
+
+  // {
+  //   id: 1,
+  //   title: "나이키 에어조던1 스캇 팬텀블랙 265",
+  //   period: "2023.05.01 ~ 2023.05.15",
+  //   bidding: 5,
+  //   auctionState: true,
+  //   heart: 5,
+  //   img: [img1, img2, img3],  //   userName: "당근단군",
+  //   userLocation: "군자동",
+  //   userImg: img2,
+  //   auctionTitle: "나이키 에어조던1 스캇 팬텀블랙 265",
+  //   auctionTime: "50분 전",
+  //   content:
+  //     "나이키 공홈에서 당첨된 나이키 에어조던1 스캇입니다!\n사진 찍기 위하여 보자기만 걷어봤고 그 외에는 시착도 안 했습니다.\n건대입구에서 직거래 희망합니다!",
+  //   auctionSummary: "입찰 3 ・ 관심 1 ・ 조회 146",
+  //   auctionEnd: "2023년 5월 15일 오후 5시 26분",
+  // },
+
   return (
     <Wrapper>
       <AuctionImgContainer>
-        <ItemImage images={dummyImages} />
+        <ItemImage images={dummyItem[id].img} />
+        <BackButton onClick={handleBack} />
       </AuctionImgContainer>
+
       <UserInfoContainer>
-        <UserImg src={dummyAuctions.userImg} />
+        <UserImg src={dummyItem[id].userImg} />
         <UserText>
-          <div>{dummyAuctions.userName}</div>
-          <div className="userLocation">{dummyAuctions.userLocation}</div>
+          <div>{dummyItem[id].userName}</div>
+          <div className="userLocation">{dummyItem[id].userLocation}</div>
         </UserText>
       </UserInfoContainer>
-      <UserInfoUnderLine />
-      <AuctionTitle>{dummyAuctions.auctionTitle} </AuctionTitle>
-      <AutcionInfo>{dummyAuctions.auctionTime}</AutcionInfo>
-      <AuctionContent>{dummyAuctions.content}</AuctionContent>
-      <AuctionSummary>{dummyAuctions.auctionSummary}</AuctionSummary>
-      <AuctionUnderLine />
+      <UnderLine />
+      <AuctionTitle>{dummyItem[id].auctionTitle} </AuctionTitle>
+      <AutcionInfo>{dummyItem[id].auctionTime}</AutcionInfo>
+      <AuctionContent>{dummyItem[id].content}</AuctionContent>
+      <AuctionSummary>{dummyItem[id].auctionSummary}</AuctionSummary>
+      <UnderLine />
       <BiddingList>입찰 목록</BiddingList>
       <BiddingItemGrid>
         {dummyBiddings.map((item) => (
-          <BiddingItem key={item.id}>
-            <ItemImg src={item.img} />
+          <BiddingItem
+            key={item.id}
+            onClick={() => handleToBiddingDetail(item.id)}
+          >
+            <ItemImg src={item.img[0]} />
             <ItemTitle>{item.itemTitle}</ItemTitle>
           </BiddingItem>
         ))}
@@ -39,7 +75,7 @@ const AuctionDetail = () => {
           <DivisionLine />
           <AuctionEnd>
             <div>경매 마감일</div>
-            <div style={{ color: "gray" }}>{dummyAuctions.auctionEnd}</div>
+            <div style={{ color: "gray" }}>{dummyItem[id].auctionEnd}</div>
           </AuctionEnd>
           <BiddingButton>입찰하기</BiddingButton>
         </FooterContainer>
@@ -62,6 +98,16 @@ const AuctionImgContainer = styled.div`
   padding-top: 100%;
 `;
 
+const BackButton = styled(IoArrowBackOutline)`
+  position: absolute;
+  top: 2.5%;
+  left: 2.5%;
+  font-size: 2rem;
+  z-index: 10;
+  color: #f0f0f0;
+  cursor: pointer;
+`;
+
 const UserInfoContainer = styled.div`
   height: 5rem;
   display: flex;
@@ -71,7 +117,6 @@ const UserImg = styled.img`
   width: 3rem;
   height: 3rem;
   margin: 1rem 0.5rem 1rem 1rem;
-  /* border: 0.5px solid black; */
   border: none;
   border-radius: 50%;
   object-fit: cover;
@@ -89,11 +134,6 @@ const UserText = styled.div`
     font-size: 0.75rem;
     color: gray;
   }
-`;
-
-const UserInfoUnderLine = styled.div`
-  border: 0.5px solid #cccccc;
-  margin: 0 1rem;
 `;
 
 const AuctionTitle = styled.div`
@@ -114,22 +154,21 @@ const AuctionContent = styled.div`
 `;
 
 const AuctionSummary = styled.div`
-  margin-left: 1rem;
+  margin: 0 0 1.5rem 1rem;
   font-size: 0.75rem;
   font-weight: bold;
   color: gray;
 `;
 
-const AuctionUnderLine = styled.div`
+const UnderLine = styled.div`
   border: 0.5px solid #cccccc;
-  margin: 1.5rem 1rem;
+  margin: 0 1rem;
 `;
 
 const BiddingList = styled.div`
   font-weight: bold;
   font-size: 1.2rem;
-  margin-left: 1rem;
-  margin-bottom: 1.5rem;
+  margin: 1.5rem 1rem;
 `;
 
 const BiddingItemGrid = styled.div`
