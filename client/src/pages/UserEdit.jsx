3d72cf555020618492370d52734e4dadac4846ee
jong-bak <120023946+jong-bak@users.noreply.github.com>
2023-05-09 19:07:57 +0900

    test: commit

--- client/src/pages/UserEdit.jsx
+++ client/src/pages/UserEdit.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components"
+import UserEditHeader from '../components/UI/Header/UserEditHeader';
 
-const UserEdit = () => {
-  return <div>UserEdit</div>;
+export default function UserEdit(){
+  return (
+    <Wrapper>
+      <UserEditHeader title={"내 정보 수정"}/>
+      <Line/>
+    </Wrapper>
+  )
 };
 
-export default UserEdit;
+const Wrapper = styled.div`  
+`
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background: #f2f3f6;
+`
+
+
