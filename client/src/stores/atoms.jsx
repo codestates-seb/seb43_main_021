d4826ff77bef9cc42d18a7280ea6d21cdcfa2a06
taeyoung1012 <dlaxodud12@naver.com>
2023-05-11 17:43:47 +0900

    [Fix] atoms 오류 수정

--- client/src/stores/atoms.jsx
+++ client/src/stores/atoms.jsx
@@ -2,24 +2,17 @@ import { atom } from "recoil";
 
 //modal on/off기능
 export const modalState = atom({
-<<<<<<< HEAD
-  key:"modalState",
-  default:false,
-});
-// 회원탈퇴 페이지에서 확인을 누르면 탈퇴페이지로 이동
-export const moveModalState = atom({
-  key:'moveModalState',
-  default:false,
-})
-
-//LogOutModal on/off기능
-export const LogOutModalState = atom({
-  key:"LogOutModalState",
-  default:false,
-});
-
-=======
   key: "modalState",
   default: false,
 });
->>>>>>> 8781c54bf9086500e79b8afd1678ac0b0c40f5e2
+// 회원탈퇴 페이지에서 확인을 누르면 탈퇴페이지로 이동
+export const moveModalState = atom({
+  key: "moveModalState",
+  default: false,
+});
+
+//LogOutModal on/off기능
+export const LogOutModalState = atom({
+  key: "LogOutModalState",
+  default: false,
+});
