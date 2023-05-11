8556b4dc91e7593f07fc127d3faa0cf8c02cc3f2
dobbykong <wlk159@naver.com>
2023-05-11 17:39:59 +0900

    물품 등록 페이지 수정

--- client/src/pages/CreateAuction.jsx
+++ client/src/pages/CreateAuction.jsx
@@ -1,48 +1,139 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { IoIosCamera } from "react-icons/io";
+import { SlClose } from "react-icons/sl"
 import { BsChevronLeft } from "react-icons/bs";
 
 const CreateAuction = () => {
 
   const navigate = useNavigate();
 
+  const [imageSrcList, setImeageSrcList] = useState([]); // 이미지 등록
+  const [isImageUploaded, setIsImageUploaded] = useState(false); // 색상을 바꾸기 위한 상태 추가
+  const [auctionPeriod, setAuctionPeriod] = useState('');
+
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+  const [showTitleWarning, setShowTitleWarning] = useState(false);
+  const [showTextWarning, setShowTextWarning] = useState(false);
+  const [showPeriodWarning, setShowPeriodWarning] = useState(false);
+
+  const onUpload = (event) => {
+    if (imageSrcList.length >= 10) {
+      return;
+    }
+    const files = event.target.files;
+    const newImageSrcList = [];
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      newImageSrcList.push(URL.createObjectURL(file));
+    }
+
+    setImeageSrcList((prevList) => [...prevList, ...newImageSrcList]);
+    setIsImageUploaded(true);
+  };
+
+  const onDeleteImage = (index) => {
+    setImeageSrcList((prevList) => {
+      const newList = [...prevList];
+      newList.splice(index, 1);
+      return newList;
+    });
+  };
+
+  const enterNumbersOnly = (event) => {
+    let input = event.target.value;
+    input = input.replace(/\D/g, '');
+
+    if (input !== '') {
+      const parsedInput = parseInt(input, 10);
+      if (parsedInput >= 1 && parsedInput <= 30) {
+        setAuctionPeriod(parsedInput.toString());
+        setShowPeriodWarning(false);
+      } else {
+        setAuctionPeriod('');
+      }
+    } else {
+      setAuctionPeriod('');
+      }
+  };
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+    setShowTitleWarning(false);
+  };
+  
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+    setShowTextWarning(false);
+  };
+
+  const handleCreateBtnClick = () => {
+    if (title === '') {
+      setShowTitleWarning(true);
+    }
+  
+    if (text === '') {
+      setShowTextWarning(true);
+    }
+  
+    if (auctionPeriod === '') {
+      setShowPeriodWarning(true);
+    }
+  
+    if (title !== '' && text !== '' && auctionPeriod !== '') {
+    }
+  };
+
   return (
     <Wrapper>
       <Header>
-        <BackButton onClick={() => navigate('/')} /> {/* onClick={() => navigate{"/Home"}} 이벤트 넣기 */}
+        <BackButton onClick={() => navigate('/')} />
         <HeaderTitle>
           <h2>경매 등록하기</h2>
         </HeaderTitle>
-        <CreateBtn><h2>완료</h2></CreateBtn> {/* h2에 onClick={} 이벤트 넣기 */}
+        <CreateBtn onClick={handleCreateBtnClick}><h2>완료</h2></CreateBtn> {/* h2에 onClick={} 이벤트 넣기 */}
       </Header>
       <Container>
       <Body>
         <AddImages>
-          <label>
-            <IoIosCamera className="icon" />
-          </label>
-          <input type="file" id="file" />
-          {/* 이미지 등록시 저장되는 곳 */}
-          {/* <img src={imgs} alt="이미지 미리보기" */}
+          <div>
+            <label htmlFor="file">
+              <IoIosCamera />
+              <h5>
+                <span style={{color: isImageUploaded && imageSrcList.length> 0 ? 'red' : 'inherit'}}>{imageSrcList.length}</span>/10</h5>
+            </label>
+            <input type="file" id="file" onChange={onUpload} multiple accept="image/*" disabled={imageSrcList.length >= 10} />
+          </div>
+          <ImageList className="image-preview">
+            {imageSrcList.map((src, index) => (
+              <ImageWrapper key={index}>
+                <img src={src} alt={"이미지 미리보기"} />
+                <DeleteButton onClick={() => onDeleteImage(index)}>
+                  <SlClose />
+                </DeleteButton>
+              </ImageWrapper>
+          ))}
+          </ImageList>
         </AddImages>
         <Title>
-          <input placeholder="제목" />
+          <input placeholder="제목" value={title} onChange={handleTitleChange} />
+          {showTitleWarning && <Warning>제목을 입력해주세요.</Warning>}
         </Title>
         <TextArea>
-          <textarea placeholder="경매 등록할 게시글의 내용을 입력해주세요." />
+          <textarea placeholder="경매 등록할 게시글의 내용을 입력해주세요." value={text} onChange={handleTextChange} />
+          {showTextWarning && <Warning>게시글 내용을 입력해주세요.</Warning>}
         </TextArea>
         <AuctionPeriod>
           <h2>경매 기간</h2>
           <h4>※ 경매 기간은 최소 1일부터 30일까지 설정할 수 있어요.</h4>
           <PeriodData>
-            <TensData>
-            <input type="number" min="0" max="3"></input>
-            </TensData>
-            <UnitsData>
-            <input type="number" min="0" max="9"></input>
-            </UnitsData>
+            <PeriodInput>
+              <input type="text" value={auctionPeriod} onChange={enterNumbersOnly} />
+              {showPeriodWarning && <Warning>경매 기간을 입력해주세요.</Warning>}
+            </PeriodInput>
             <h2>일</h2>
           </PeriodData>
         </AuctionPeriod>
@@ -87,6 +178,7 @@ const HeaderTitle = styled.div`
 
 const CreateBtn = styled.div`
   margin-left: auto;
+  cursor: pointer;
   h2 {
     color: #ff7e36;
     font-size: 15px;
@@ -94,7 +186,6 @@ const CreateBtn = styled.div`
 `;
 
 const Container = styled.div`
-
 `;
 
 const Body = styled.div`
@@ -102,23 +193,69 @@ const Body = styled.div`
 `;
 
 const AddImages = styled.div`
+  display: flex;
+  align-items: center;
   padding: 0.5rem 1rem 1.5rem 0;
   border-bottom: 1px solid #F3F3F3;
 
-  .icon {
-    font-size: 35px;
+  > div {
+    display: flex;
+    align-items: center;
+    margin-right: 1rem;
   }
 
   label {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 4.8rem;
+    height: 4.8rem;
+    background: #F2F3F7;
+    border-radius: 5%;
     cursor: pointer;
+
+    > svg {
+        width: 24px;
+        height: 24px;
+      }
   }
 
   input[type="file"] {
-    display: none;
+      display: none;
+  }
+
+  img {
+      width: 4.8rem;
+      height: 4.7rem;
+      margin-left: 1rem;
+      border-radius: 5%;
   }
 `;
 
-  const Title = styled.div`
+const ImageList = styled.div`
+  display: flex;
+  overflow-x: auto;
+`;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const DeleteButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: transparent;
+  border: none;
+  font-size: 20px;
+  color: red;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+const Title = styled.div`
   padding: 2rem 0rem;
   border-bottom: 1px solid #F3F3F3;
   outline: none;
@@ -132,7 +269,7 @@ const AddImages = styled.div`
   }
   `;
 
-  const TextArea = styled.div`
+const TextArea = styled.div`
   padding: 2.2rem 0 1.5rem 0;
   border-bottom: 1px solid #F3F3F3;
 
@@ -153,7 +290,7 @@ const AddImages = styled.div`
   }
   `;
 
-  const AuctionPeriod = styled.div`
+const AuctionPeriod = styled.div`
     h2 {
       padding-top: 1.2rem;
       padding-bottom: 0.8rem;
@@ -165,46 +302,41 @@ const AddImages = styled.div`
     }
   `;
 
-  const PeriodData = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    
-    h2 {
-      padding-top: 3rem;
-      font-size: 40px;
-    }
-  `;
+const PeriodData = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
 
-  const TensData = styled.div`
-    padding-top: 3rem;
-    padding-right: 1rem;
+  h2 {
+    font-size: 20px;
+    margin: 0;
+    padding-right: 0.5rem;
+  }
+`;
 
-    input {
-      border-bottom: teal 1px solid;
-      border-right: medium none;
-      border-left: medium none;
-      border-top: medium none;
-      outline: none;
-      text-align: center;
-      width: 4.4rem;
-      height: 4rem;
-      font-size: 50px;
-    }
-  `;
+const PeriodInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-  const UnitsData = styled.div`
-    padding-top: 3rem;
-    padding-right: 1rem;
-    input {
-      border-bottom: teal 1px solid;
-      border-right: medium none;
-      border-left: medium none;
-      border-top: medium none;
-      outline: none;
-      text-align: center;
-      width: 4.4rem;
-      height: 4rem;
-      font-size: 50px;
-    }
-  `;
+  input {
+    border-bottom: teal 1px solid;
+    border-right: medium none;
+    border-left: medium none;
+    border-top: medium none;
+    outline: none;
+    text-align: center;
+    width: 4.4rem;
+    height: 4rem;
+    font-size: 50px;
+  }
+`;
+
+const Warning = styled.div`
+  color: red;
+  font-size: 13px;
+  text-align: center;
+  margin-top: 0.5rem;
+`;
