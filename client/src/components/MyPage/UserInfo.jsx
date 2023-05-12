e924a070898a49a0d2b42cbab236491d83f12b9d
taeyoung1012 <dlaxodud12@naver.com>
2023-05-13 03:58:21 +0900

    [Feat] 랜딩 페이지 제작 중

--- client/src/components/MyPage/UserInfo.jsx
+++ client/src/components/MyPage/UserInfo.jsx
@@ -1,81 +1,77 @@
 import React from "react";
 import styled from "styled-components";
 import { BiInfoCircle } from "react-icons/bi";
-import { RiLockPasswordFill } from "react-icons/ri"
+import { RiLockPasswordFill } from "react-icons/ri";
 import { BiLogOut } from "react-icons/bi";
 import { BsPeople } from "react-icons/bs";
-import { Link } from 'react-router-dom';
-import { useRecoilState } from "recoil"
-import { 
-  modalState,
-  LogOutModalState,
-} from '../../stores/atoms';
-export default function UserInfo(){  
-  const [isOpen, setIsOpen] = useRecoilState(modalState);  
-  const [logOutClick, setLogOutClick] = useRecoilState(LogOutModalState);  
+import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { modalState, LogOutModalState } from "../../stores/atoms";
+export default function UserInfo() {
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
+  const [logOutClick, setLogOutClick] = useRecoilState(LogOutModalState);
 
-  
-  const openModalHandler = ()=>{
-    setIsOpen(!isOpen)    
-  }
-  const logOutModalHandler=()=>{
-    setLogOutClick(!logOutClick)
-  }
-  return(
-    <Wrapper>      
+  const openModalHandler = () => {
+    setIsOpen(!isOpen);
+  };
+  const logOutModalHandler = () => {
+    setLogOutClick(!logOutClick);
+  };
+  return (
+    <Wrapper>
       나의 활동
       <MySeletorContainer>
-        <StyledLink to='/useredit'>
+        <StyledLink to="/useredit">
           <IconContainer>
-            <BiInfoCircle/>
-            내 정보수정
+            <BiInfoCircle />내 정보수정
           </IconContainer>
         </StyledLink>
         <IconContainer>
-          <RiLockPasswordFill/>
+          <RiLockPasswordFill />
           비밀번호 변경
         </IconContainer>
         <IconContainer onClick={logOutModalHandler}>
-          <BiLogOut/>
+          <BiLogOut />
           로그아웃
         </IconContainer>
         <IconContainer onClick={openModalHandler}>
-          <BsPeople/>회원탈퇴
+          <BsPeople />
+          회원탈퇴
         </IconContainer>
       </MySeletorContainer>
     </Wrapper>
-  )
+  );
 }
 
 const Wrapper = styled.div`
   display: flex;
   width: 100%;
-  height: 29.25rem;  
+  height: 29.25rem;
   padding-left: 1rem;
   margin-top: 1.25rem;
   font-size: 15px;
   font-weight: bold;
-  text-decoration: none;     
-  svg{
+  text-decoration: none;
+  svg {
     width: 24px;
     height: 24px;
     margin-right: 1rem;
     cursor: pointer;
   }
-`
+`;
 
 const MySeletorContainer = styled.div`
   margin-left: 0.25rem;
   position: absolute;
   margin-top: 4rem;
-`
+`;
 const IconContainer = styled.div`
-  margin-bottom: 1.75rem;  
+  margin-bottom: 1.75rem;
   display: flex;
-  align-items: center;   
-  color: black;   
-  cursor: pointer;    
-`
+  align-items: center;
+  color: black;
+  cursor: pointer;
+`;
 const StyledLink = styled(Link)`
   text-decoration: none;
 `;
