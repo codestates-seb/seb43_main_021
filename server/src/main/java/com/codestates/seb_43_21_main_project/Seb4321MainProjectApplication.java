e88697aed2d09f0eb819825f708bfca8aef002fe
min4415 <csm4415@gmail.com>
2023-05-12 15:33:35 +0900

    er

--- server/src/main/java/com/codestates/seb_43_21_main_project/Seb4321MainProjectApplication.java
+++ server/src/main/java/com/codestates/seb_43_21_main_project/Seb4321MainProjectApplication.java
@@ -1,5 +1,6 @@
 package com.codestates.seb_43_21_main_project;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Seb4321MainProjectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Seb4321MainProjectApplication.class, args);
 	}
 
