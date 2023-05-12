36bf1e0d9230f34475b65f45af6689880708c7cd
ye2un <thdwlfwn47@gmail.com>
2023-05-12 15:40:02 +0900

    [refactor] AuctionItem 필드 수정

--- server/build/generated/sources/annotationProcessor/java/main/com/codestates/seb_43_21_main_project/auctionItem/mapper/AuctionMapperImpl.java
+++ server/build/generated/sources/annotationProcessor/java/main/com/codestates/seb_43_21_main_project/auctionItem/mapper/AuctionMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-10T01:38:26+0900",
+    date = "2023-05-12T15:03:12+0900",
     comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.1.jar, environment: Java 11.0.17 (Azul Systems, Inc.)"
 )
 @Component
@@ -29,7 +29,6 @@ public class AuctionMapperImpl implements AuctionMapper {
         auction.name( requestBody.getName() );
         auction.content( requestBody.getContent() );
         auction.period( requestBody.getPeriod() );
-        auction.categoryId( requestBody.getCategoryId() );
         auction.auctionStatus( requestBody.getAuctionStatus() );
 
         return auction.build();
