78de9ba9d137fe6bfaec96da53ebc3c5855dbe7b
min4415 <csm4415@gmail.com>
2023-05-16 16:27:29 +0900

    ACCESS_NOT_ALLOWED 추가

--- server/src/main/java/com/codestates/seb_43_21_main_project/exception/ExceptionCode.java
+++ server/src/main/java/com/codestates/seb_43_21_main_project/exception/ExceptionCode.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다."),
-    MEMBER_EXISTS(404, "중복된 닉네임입니다.");
+    MEMBER_EXISTS(404, "중복된 닉네임입니다."),
+    ACCESS_NOT_ALLOWED(403, "권한이 없습니다.");
+
     @Getter
     private int status;
 
