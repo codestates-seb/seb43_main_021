f2baea3fddf33b434bc4bc3f153af8e39cab754b
ye2un <120310641+ye2un@users.noreply.github.com>
2023-05-18 15:10:17 +0900

    Delete server directory

--- server/src/main/java/com/codestates/seb_43_21_main_project/auctionItem/service/StorageService.java
+++
@@ -1,7 +0,0 @@
-package com.codestates.seb_43_21_main_project.auctionItem.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface StorageService {
-    void store(MultipartFile file);
-}
