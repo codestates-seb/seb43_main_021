068feb04df6e815e0b4a5e9783003a1ad28a5353
taeyoung1012 <dlaxodud12@naver.com>
2023-05-24 21:01:03 +0900

    컨플릭 해결 중

--- client/src/components/Login/LoginForm.js
+++ client/src/components/Login/LoginForm.js
@@ -45,15 +45,14 @@ const LoginFrom = () => {
           if (res.data.result === "success") {
             const accessToken = res.headers.authorization;
             const refreshToken = res.headers.refresh;
-            const memberId = res.headers.memberid;
+            const memberId = res.headers.memberId;
             console.log("로그인 성공:", res.data, res.headers);
 
             localStorage.setItem("accessToken", accessToken);
             localStorage.setItem("refreshToken", refreshToken);
-            localStorage.setItem("memberId", memberId);
-
             setKeepLoggedIn(true);
-            navigate("/main");
+            localStorage.setItem("memberId", memberId);
+            // navigate("/main");
           } else {
             setErrorMessage("이메일 또는 비밀번호가 올바르지 않습니다.");
           }
