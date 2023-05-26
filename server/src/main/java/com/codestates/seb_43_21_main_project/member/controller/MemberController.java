6aa42e5e4d977db18da6f357daffe44b22207454
jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
2023-05-26 13:31:18 +0900

    [feature] aws cors문제

--- server/src/main/java/com/codestates/seb_43_21_main_project/member/controller/MemberController.java
+++ server/src/main/java/com/codestates/seb_43_21_main_project/member/controller/MemberController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "true")
+@CrossOrigin(origins = "*")
 @Validated
 @RestController
 @RequestMapping("/member")
