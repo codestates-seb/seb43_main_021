252f6c6f3d84d5df0272083d488aca8a80df2fad
jae-jung-kim <podosi158@gmail.com>
2023-05-26 15:33:01 +0900

    [feature] aws cors 문제 webconfig 수정

--- server/src/main/java/com/codestates/seb_43_21_main_project/security/config/WebConfig.java
+++ server/src/main/java/com/codestates/seb_43_21_main_project/security/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.codestates.seb_43_21_main_project.security.config;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
