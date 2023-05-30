ac1971ffd89cd07ce4c9fb5070964b2e7a2ce13c
dobbykong <wlk159@naver.com>
2023-05-30 18:28:33 +0900

    [feat] 알림 기능 추가

--- client/src/components/Alarm/AlarmItem.js
+++ client/src/components/Alarm/AlarmItem.js
@@ -1,42 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { RiAuctionFill } from "react-icons/ri";
-import { AiFillMessage } from "react-icons/ai";
+import { useRecoilState } from "recoil";
+import { profileNicknameState } from "../../stores/atoms";
 
-const AlarmItem = ( { type, username } ) => {
+const AlarmItem = () => {
+  const [isDeleted, setIsDeleted] = useState(false);
+  const [userNickname, setUserNickname] = useRecoilState(profileNicknameState);
 
-    return (
-        <Container>
-            {type === "공지" && (
-                <Icon>
-                    <RiAuctionFill />
-                </Icon>
-            )}
-            {type === "채팅" && (
-                <Icon>
-                    <AiFillMessage />
-                </Icon>
-            )}
-            <AlarmMessage>
-                {type === "공지" ? "니꺼내꺼에 오신것을 환영합니다!" : `${username}님이 메시지를 보냈습니다.`}
-            </AlarmMessage>
-            <DeletedBtn>
+  const onClickDelete = () => {
+    setIsDeleted(true);
+  };
 
-            </DeletedBtn>
-        </Container>
-    );
+  if (isDeleted) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <AlarmMessage>
+        <div>
+          <label>
+            <RiAuctionFill />
+          </label>
+        </div>
+        <MessageContent>
+          {userNickname}님! 니꺼내꺼 회원가입을 진심으로 환영합니다!
+        </MessageContent>
+        <DeletedBtn>
+          <button onClick={onClickDelete}>X</button>
+        </DeletedBtn>
+      </AlarmMessage>
+    </Container>
+  );
 };
 
 export default AlarmItem;
 
 const Container = styled.div`
-`;
-
-const Icon = styled.div`
+  width: 100%;
 `;
 
 const AlarmMessage = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0.5rem 1rem 1.5rem 0;
+
+  > div {
+    display: flex;
+    align-items: center;
+    margin-right: 1rem;
+  }
+
+  label {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 3rem;
+    height: 3rem;
+    background: var(--orange1-color);
+    border-radius: 50%;
+
+    > svg {
+      width: 20px;
+      height: 20px;
+      color: var(--white1-color);
+    }
+  }
+`;
+
+const MessageContent = styled.h4`
+  padding-left: 1rem;
+  width: 100%;
 `;
 
 const DeletedBtn = styled.div`
+  button {
+    border: none;
+    cursor: pointer;
+    background-color: var(--white1-color);
+    font-size: 16px;
+    font-weight: bold;
+  }
 `;
